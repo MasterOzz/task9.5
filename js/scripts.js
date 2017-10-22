e8d3227db311cc4b8f45aec82cbe6f3983171332
MasterOzz <32138364+MasterOzz@users.noreply.github.com>
2017-10-22 22:01:18 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
 var buttonAlertclass = document.getElementsByClassName('button');
-console.log(buttonAlertclass);
 
-
-function displayButtonText (element) {
-        alert(element.innerHTML);
-}
 	for (var i = 0; i < buttonAlertclass.length; i++) {
-   		displayButtonText(buttonAlertclass[i]);
+   		alert(buttonAlertclass[i].innerHTML);
 }
